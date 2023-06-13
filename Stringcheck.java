bc9d161f74ee95dad1d049b7ed6605e420ce4ee8
pk65arya <pk65arya@gmail.com>
2023-06-13 09:52:44 +0530

    Added:java files

---
+++ Stringcheck.java
@@ -0,0 +1,13 @@
+package com.basicjava;
+
+public class Stringcheck {
+    public static void main(String[] args) {
+        String s1="my name is pradeep";
+        String s2="my name is pradeep";
+        if(s1==s2){
+            System.out.println("the strings are equal");
+        }else {
+            System.out.println("the string are not equal");
+        }
+    }
+}
