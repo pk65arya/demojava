232dd9417ad23b3c8f620737ea544526bee8a9ba
pk65arya <pk65arya@gmail.com>
2023-06-16 09:05:12 +0530

    Added:java file

---
+++ MaxandMin.java
@@ -0,0 +1,17 @@
+package com.basicjava;
+import java.util.*;
+import java.util.Scanner;
+
+public class MaxandMin {
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        if(x>y){
+            System.out.println("maximum is :" +x);
+            System.out.println("minium is :" +y);
+        }else{
+            System.out.println("the maximum is: " +y);
+            System.out.println("thi minimum is : " +x);
+        }
+    }
+}
