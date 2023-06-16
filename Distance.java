1f6ff1d3eb9ac4285e077be03e4e48f21e21554d
pk65arya <pk65arya@gmail.com>
2023-06-16 08:54:45 +0530

    Added:java file

--- Distance.java
+++ Distance.java
@@ -3,11 +3,13 @@ package com.basicjava;
 import java.util.Scanner;
 
 public class Distance {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        double distance=Math.sqrt((x*x)+(y*y));
+    public static void main(String args[]) {
+
+
+
+      int  x=Integer.parseInt(args[0]);
+       int  y=Integer.parseInt(args[1]);
+       double  distance=Math.sqrt((x*x)+(y*y));
         System.out.println("the distance between the given points is: "+distance);
     }
 }
